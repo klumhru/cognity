0f7b584d4ac59c523071eb5ba8a7ddbbca299749
Högni Gylfason <klumhru@gmail.com>
2020-09-07 13:49:14 -0400

    fooooo

--- Assets/Packages/Cognity/Demo/ProfileUI.cs
+++ Assets/Packages/Cognity/Demo/ProfileUI.cs
@@ -7,15 +7,6 @@ namespace Cognity.Demo {
   public class ProfileUI : MonoBehaviour {
     public Profile Profile;
     public Login Login;
-    // Start is called before the first frame update
-    void Start() {
-
-    }
-
-    // Update is called once per frame
-    void Update() {
-
-    }
 
     public void OnSignOutClick() {
       Login.Signout();
